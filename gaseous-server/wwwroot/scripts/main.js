6377a99583a9e773ad3b90cacad6a12836376290
Michael Green <84688932+michael-j-green@users.noreply.github.com>
2023-06-29 07:47:33 +1000

    feat: added ROM info modal

--- gaseous-server/wwwroot/scripts/main.js
+++ gaseous-server/wwwroot/scripts/main.js
@@ -34,3 +34,40 @@ function formatBytes(bytes, decimals = 2) {
 
     return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`
 }
+
+function showDialog(dialogPage, variables) {
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the modal content
+    var modalContent = document.getElementById("modal-content");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal 
+    modal.style.display = "block";
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+        modalContent.innerHTML = "";
+        modalVariables = null;
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+            modalContent.innerHTML = "";
+            modalVariables = null;
+        }
+    }
+
+    modalVariables = variables;
+
+    $('#modal-content').load('/pages/dialogs/' + dialogPage + '.html');
+}
