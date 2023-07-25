8a001f9fa4977e729ca866eeba7d938f92c9c671
Michael Green <84688932+michael-j-green@users.noreply.github.com>
2023-07-25 21:26:59 +1000

    feat: added platform disk usage breakdown on the settings page (#37)

--- gaseous-server/Controllers/SystemController.cs
+++ gaseous-server/Controllers/SystemController.cs
@@ -14,35 +14,73 @@ namespace gaseous_server.Controllers
     {
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public Dictionary<string, object> GetSystemStatus()
+        public SystemInfo GetSystemStatus()
         {
             Database db = new gaseous_tools.Database(Database.databaseType.MySql, Config.DatabaseConfiguration.ConnectionString);
 
-            Dictionary<string, object> ReturnValue = new Dictionary<string, object>();
+            SystemInfo ReturnValue = new SystemInfo();
 
             // disk size
-            List<Dictionary<string, object>> Disks = new List<Dictionary<string, object>>();
+            List<SystemInfo.PathItem> Disks = new List<SystemInfo.PathItem>();
             //Disks.Add(GetDisk(gaseous_tools.Config.ConfigurationPath));
             Disks.Add(GetDisk(gaseous_tools.Config.LibraryConfiguration.LibraryRootDirectory));
-            ReturnValue.Add("Paths", Disks);
+            ReturnValue.Paths = Disks;
 
             // database size
             string sql = "SELECT table_schema, SUM(data_length + index_length) FROM information_schema.tables WHERE table_schema = '" + Config.DatabaseConfiguration.DatabaseName + "'";
             DataTable dbResponse = db.ExecuteCMD(sql);
-            ReturnValue.Add("DatabaseSize", dbResponse.Rows[0][1]);
+            ReturnValue.DatabaseSize = (long)(System.Decimal)dbResponse.Rows[0][1];
+
+            // platform statistics
+            sql = "SELECT Platform.`name`, grc.Count, grs.Size FROM Platform INNER JOIN (SELECT Platform.`name` AS `Name`, SUM(grs.Size) AS Size FROM Platform JOIN Games_Roms AS grs ON (grs.PlatformId = Platform.Id) GROUP BY Platform.`name`) grs ON (grs.`Name` = Platform.`name`) INNER JOIN (SELECT Platform.`name` AS `Name`, COUNT(grc.Size) AS Count FROM Platform JOIN Games_Roms AS grc ON (grc.PlatformId = Platform.Id) GROUP BY Platform.`name`) grc ON (grc.`Name` = Platform.`name`) ORDER BY Platform.`name`;";
+            dbResponse = db.ExecuteCMD(sql);
+            ReturnValue.PlatformStatistics = new List<SystemInfo.PlatformStatisticsItem>();
+            foreach (DataRow dr in dbResponse.Rows)
+            {
+                SystemInfo.PlatformStatisticsItem platformStatisticsItem = new SystemInfo.PlatformStatisticsItem
+                {
+                    Platform = (string)dr["name"],
+                    RomCount = (long)dr["Count"],
+                    TotalSize = (long)(System.Decimal)dr["Size"]
+                };
+                ReturnValue.PlatformStatistics.Add(platformStatisticsItem);
+            }
 
             return ReturnValue;
         }
 
-        private Dictionary<string, object> GetDisk(string Path)
+        private SystemInfo.PathItem GetDisk(string Path)
         {
-            Dictionary<string, object> DiskValues = new Dictionary<string, object>();
-            DiskValues.Add("LibraryPath", Path);
-            DiskValues.Add("SpaceUsed", gaseous_tools.Common.DirSize(new DirectoryInfo(Path)));
-            DiskValues.Add("SpaceAvailable", new DriveInfo(Path).AvailableFreeSpace);
-            DiskValues.Add("TotalSpace", new DriveInfo(Path).TotalSize);
+            SystemInfo.PathItem pathItem = new SystemInfo.PathItem {
+                LibraryPath = Path,
+                SpaceUsed = gaseous_tools.Common.DirSize(new DirectoryInfo(Path)),
+                SpaceAvailable = new DriveInfo(Path).AvailableFreeSpace,
+                TotalSpace = new DriveInfo(Path).TotalSize
+            };
 
-            return DiskValues;
+            return pathItem;
+        }
+
+        public class SystemInfo
+        {
+            public List<PathItem>? Paths { get; set; }
+            public long DatabaseSize { get; set; }
+            public List<PlatformStatisticsItem>? PlatformStatistics { get; set; }
+
+            public class PathItem
+            {
+                public string LibraryPath { get; set; }
+                public long SpaceUsed { get; set; }
+                public long SpaceAvailable { get; set; }
+                public long TotalSpace { get; set; }
+            }
+
+            public class PlatformStatisticsItem
+            {
+                public string Platform { get; set; }
+                public long RomCount { get; set; }
+                public long TotalSize { get; set; }
+            }
         }
     }
 }
--- gaseous-server/wwwroot/scripts/main.js
+++ gaseous-server/wwwroot/scripts/main.js
@@ -100,3 +100,20 @@ function createTableRow(isHeader, row, rowClass, cellClass) {
 
     return newRow;
 }
+
+function hashCode(str) {
+    var hash = 0;
+    for (var i = 0; i < str.length; i++) {
+        hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+
+    return hash;
+}
+
+function intToRGB(i) {
+    var c = (i & 0x00FFFFFF)
+        .toString(16)
+        .toUpperCase();
+
+    return "00000".substring(0, 6 - c.length) + c;
+}
