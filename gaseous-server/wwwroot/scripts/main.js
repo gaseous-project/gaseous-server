28823d940f6b3a6ea38755b54b151bd518deb53a
Michael Green <84688932+michael-j-green@users.noreply.github.com>
2023-09-02 01:33:55 +1000

    Filter settings are now stored in a session cookie (#80)

--- gaseous-server/wwwroot/scripts/main.js
+++ gaseous-server/wwwroot/scripts/main.js
@@ -57,6 +57,33 @@ function getQueryString(stringName, type) {
     }
 }
 
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    if (exdays) {
+        let expires = "expires="+ d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    } else {
+        document.cookie = cname + "=" + cvalue + ";path=/";
+    }
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
+
 function formatBytes(bytes, decimals = 2) {
     if (!+bytes) return '0 Bytes'
 
