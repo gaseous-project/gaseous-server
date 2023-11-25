71835f2a78c0d48d618466c076660c552706b316
Michael Green <84688932+michael-j-green@users.noreply.github.com>
2023-11-26 01:38:02 +1100

    Include ROM's without a signature match in rematcher efforts

--- gaseous-server/Classes/ImportGames.cs
+++ gaseous-server/Classes/ImportGames.cs
@@ -752,9 +752,9 @@ namespace gaseous_server.Classes
             Logging.Log(Logging.LogType.Information, "Rematch Scan", "Rematch scan starting");
 
             Database db = new Database(Database.databaseType.MySql, Config.DatabaseConfiguration.ConnectionString);
-            string sql = "SELECT * FROM Games_Roms WHERE (PlatformId = 0 OR GameId = 0) AND (LastMatchAttemptDate IS NULL OR LastMatchAttemptDate < @lastmatchattemptdate) LIMIT 100;";
+            string sql = "SELECT * FROM Games_Roms WHERE (PlatformId = 0 OR GameId = 0 OR MetaddataSource = 0) AND (LastMatchAttemptDate IS NULL OR LastMatchAttemptDate < @lastmatchattemptdate) LIMIT 100;";
             Dictionary<string, object> dbDict = new Dictionary<string, object>();
-            dbDict.Add("lastmatchattemptdate", DateTime.UtcNow.AddMonths(-1));
+            dbDict.Add("lastmatchattemptdate", DateTime.UtcNow.AddDays(-7));
             DataTable data = db.ExecuteCMD(sql, dbDict);
             foreach (DataRow row in data.Rows)
             {
