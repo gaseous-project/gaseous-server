b564edb1581849c7a40c1eed0c1a881f3bf733cf
Michael Green <84688932+michael-j-green@users.noreply.github.com>
2023-12-01 13:28:41 +1100

    Background task execution intervals are now user configurable (#209)
    * Store background task intervals in database
    
    * Background task intervals are now user customisable

--- gaseous-server/Controllers/V1.0/SystemController.cs
+++ gaseous-server/Controllers/V1.0/SystemController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ using gaseous_server.Classes;
 using gaseous_server.Classes.Metadata;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.Hosting;
 
 namespace gaseous_server.Controllers
 {
@@ -99,6 +101,75 @@ namespace gaseous_server.Controllers
             return File(bytes, "text/javascript");
         }
 
+        [MapToApiVersion("1.0")]
+        [MapToApiVersion("1.1")]
+        [HttpGet]
+        [Route("Settings/BackgroundTasks/Intervals")]
+        [Authorize(Roles = "Admin")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult GetBackgroundTasks()
+        {
+            Dictionary<string, BackgroundTaskItem> Intervals = new Dictionary<string, BackgroundTaskItem>();
+            Intervals.Add(ProcessQueue.QueueItemType.SignatureIngestor.ToString(), new BackgroundTaskItem(ProcessQueue.QueueItemType.SignatureIngestor));
+            Intervals.Add(ProcessQueue.QueueItemType.TitleIngestor.ToString(), new BackgroundTaskItem(ProcessQueue.QueueItemType.TitleIngestor));
+            Intervals.Add(ProcessQueue.QueueItemType.MetadataRefresh.ToString(), new BackgroundTaskItem(ProcessQueue.QueueItemType.MetadataRefresh));
+            Intervals.Add(ProcessQueue.QueueItemType.OrganiseLibrary.ToString(), new BackgroundTaskItem(ProcessQueue.QueueItemType.OrganiseLibrary));
+            Intervals.Add(ProcessQueue.QueueItemType.LibraryScan.ToString(), new BackgroundTaskItem(ProcessQueue.QueueItemType.LibraryScan));
+            Intervals.Add(ProcessQueue.QueueItemType.Rematcher.ToString(), new BackgroundTaskItem(ProcessQueue.QueueItemType.Rematcher));
+            Intervals.Add(ProcessQueue.QueueItemType.Maintainer.ToString(), new BackgroundTaskItem(ProcessQueue.QueueItemType.Maintainer));
+
+            return Ok(Intervals);
+        }
+
+        [MapToApiVersion("1.0")]
+        [MapToApiVersion("1.1")]
+        [HttpPost]
+        [Route("Settings/BackgroundTasks/Intervals")]
+        [Authorize(Roles = "Admin")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult SetBackgroundTasks(Dictionary<string, int> Intervals)
+        {
+            foreach (KeyValuePair<string, int> Interval in Intervals)
+            {
+                if (Enum.IsDefined(typeof(ProcessQueue.QueueItemType), Interval.Key))
+                {
+                    try
+                    {
+                        BackgroundTaskItem taskItem = new BackgroundTaskItem(
+                            (ProcessQueue.QueueItemType)Enum.Parse(typeof(ProcessQueue.QueueItemType), Interval.Key)
+                        );
+
+                        if (Interval.Value >= taskItem.MinimumAllowedValue)
+                        {
+                            Logging.Log(Logging.LogType.Information, "Update Background Task", "Updating task " + Interval.Key + " with new interval " + Interval.Value);
+                            
+                            Config.SetSetting("Interval_" + Interval.Key, Interval.Value.ToString());
+
+                            // update existing process
+                            foreach (ProcessQueue.QueueItem item in ProcessQueue.QueueItems)
+                            {
+                                if (item.ItemType.ToString().ToLower() == Interval.Key.ToLower())
+                                {
+                                    item.Interval = Interval.Value;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Logging.Log(Logging.LogType.Warning, "Update Background Task", "Interval " + Interval.Value + " for task " + Interval.Key + " is below the minimum allowed value of " + taskItem.MinimumAllowedValue + ". Skipping.");
+                        }
+                    }
+                    catch
+                    {
+                        // task name not defined
+                        Logging.Log(Logging.LogType.Warning, "Update Background Task", "Task " + Interval.Key + " is not user definable. Skipping.");
+                    }
+                }
+            }
+
+            return Ok();
+        }
+
         private SystemInfo.PathItem GetDisk(string Path)
         {
             SystemInfo.PathItem pathItem = new SystemInfo.PathItem {
@@ -138,5 +209,64 @@ namespace gaseous_server.Controllers
                 public long TotalSize { get; set; }
             }
         }
+    }
+
+    public class BackgroundTaskItem
+    {
+        public BackgroundTaskItem(ProcessQueue.QueueItemType TaskName)
+        {
+            this.Task = TaskName.ToString();
+
+            switch (TaskName)
+            {
+                case ProcessQueue.QueueItemType.SignatureIngestor:
+                    this.DefaultInterval = 60;
+                    this.MinimumAllowedValue = 20;
+                    break;
+                
+                case ProcessQueue.QueueItemType.TitleIngestor:
+                    this.DefaultInterval = 1;
+                    this.MinimumAllowedValue = 1;
+                    break;
+
+                case ProcessQueue.QueueItemType.MetadataRefresh:
+                    this.DefaultInterval = 360;
+                    this.MinimumAllowedValue = 360;
+                    break;
+
+                case ProcessQueue.QueueItemType.OrganiseLibrary:
+                    this.DefaultInterval = 1440;
+                    this.MinimumAllowedValue = 120;
+                    break;
+
+                case ProcessQueue.QueueItemType.LibraryScan:
+                    this.DefaultInterval = 1440;
+                    this.MinimumAllowedValue = 120;
+                    break;
+
+                case ProcessQueue.QueueItemType.Rematcher:
+                    this.DefaultInterval = 1440;
+                    this.MinimumAllowedValue = 360;
+                    break;
+
+                case ProcessQueue.QueueItemType.Maintainer:
+                    this.DefaultInterval = 10080;
+                    this.MinimumAllowedValue = 10080;
+                    break;
+
+                default:
+                    throw new Exception("Invalid task");
+            }
+        }
+
+        public string Task { get; set; }
+        public int Interval {
+            get
+            {
+                return int.Parse(Config.ReadSetting("Interval_" + Task, DefaultInterval.ToString()));
+            }
+        }
+        public int DefaultInterval { get; set; }
+        public int MinimumAllowedValue { get; set; }
     }
 }
