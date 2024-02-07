a2d863dc7a9b3097e1c23ec60e22f3cb34948b61
Michael Green <84688932+michael-j-green@users.noreply.github.com>
2024-02-07 16:19:30 +1100

    Added game last played, and total play time stats
    * Added game last played, and total play time stats

---
+++ gaseous-server/Classes/Statistics.cs
@@ -0,0 +1,99 @@
+using System.Data;
+using gaseous_server.Models;
+
+namespace gaseous_server.Classes
+{
+    public class Statistics
+    {
+        public StatisticsModel RecordSession(Guid SessionId, long GameId, string UserId)
+        {
+            Database db = new Database(Database.databaseType.MySql, Config.DatabaseConfiguration.ConnectionString);
+            string sql;
+            Dictionary<string, object> dbDict;
+
+            if (SessionId == Guid.Empty)
+            {
+                // new session required
+                SessionId = Guid.NewGuid();
+
+                sql = "INSERT INTO UserTimeTracking (GameId, UserId, SessionId, SessionTime, SessionLength) VALUES (@gameid, @userid, @sessionid, @sessiontime, @sessionlength);";
+                dbDict = new Dictionary<string, object>{
+                    { "gameid", GameId },
+                    { "userid", UserId },
+                    { "sessionid", SessionId },
+                    { "sessiontime", DateTime.UtcNow },
+                    { "sessionlength", 1 }
+                };
+
+                db.ExecuteNonQuery(sql, dbDict);
+                
+                return new StatisticsModel{
+                    GameId = GameId,
+                    SessionId = SessionId,
+                    SessionStart = (DateTime)dbDict["sessiontime"],
+                    SessionLength = (int)dbDict["sessionlength"]
+                };
+            }
+            else
+            {
+                // update existing session
+                sql = "UPDATE UserTimeTracking SET SessionLength = SessionLength + @sessionlength WHERE GameId = @gameid AND UserId = @userid AND SessionId = @sessionid;";
+                dbDict = new Dictionary<string, object>{
+                    { "gameid", GameId },
+                    { "userid", UserId },
+                    { "sessionid", SessionId },
+                    { "sessionlength", 1 }
+                };
+
+                db.ExecuteNonQuery(sql, dbDict);
+
+                sql = "SELECT * FROM UserTimeTracking WHERE GameId = @gameid AND UserId = @userid AND SessionId = @sessionid;";
+                DataTable data = db.ExecuteCMD(sql, dbDict);
+
+                return new StatisticsModel{
+                    GameId = (long)data.Rows[0]["GameId"],
+                    SessionId = Guid.Parse(data.Rows[0]["SessionId"].ToString()),
+                    SessionStart = (DateTime)data.Rows[0]["SessionTime"],
+                    SessionLength = (int)data.Rows[0]["SessionLength"]
+                };
+            }
+        }
+
+        public StatisticsModel? GetSession(long GameId, string UserId)
+        {
+            Database db = new Database(Database.databaseType.MySql, Config.DatabaseConfiguration.ConnectionString);
+            string sql = "SELECT SUM(SessionLength) AS TotalLength FROM UserTimeTracking WHERE GameId = @gameid AND UserId = @userid;";
+            Dictionary<string, object> dbDict = new Dictionary<string, object>{
+                { "gameid", GameId },
+                { "userid", UserId }
+            };
+
+            DataTable data = db.ExecuteCMD(sql, dbDict);
+
+            if (data.Rows.Count == 0)
+            {
+                return null;
+            }
+            else
+            {
+                if (data.Rows[0]["TotalLength"] == DBNull.Value)
+                {
+                    return null;
+                }
+                else
+                {
+                    int TotalTime = int.Parse(data.Rows[0]["TotalLength"].ToString());
+
+                    sql = "SELECT * FROM UserTimeTracking WHERE GameId = @gameid AND UserId = @userid ORDER BY SessionTime DESC LIMIT 1;";
+                    data = db.ExecuteCMD(sql, dbDict);
+
+                    return new StatisticsModel{
+                        GameId = GameId,
+                        SessionLength = TotalTime,
+                        SessionStart = (DateTime)data.Rows[0]["SessionTime"]
+                    };
+                }
+            }
+        }
+    }
+}
---
+++ gaseous-server/Controllers/V1.1/StatisticsController.cs
@@ -0,0 +1,104 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using gaseous_server.Models;
+using gaseous_server.Classes;
+using Authentication;
+using Microsoft.AspNetCore.Identity;
+using Asp.Versioning;
+
+namespace gaseous_server.Controllers.v1_1
+{
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiVersion("1.0")]
+    [ApiVersion("1.1")]
+    [ApiController]
+    public class StatisticsController: ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public StatisticsController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager
+        )
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [MapToApiVersion("1.0")]
+        [MapToApiVersion("1.1")]
+        [HttpPost]
+        [Authorize]
+        [ProducesResponseType(typeof(Models.StatisticsModel), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [Route("Games/{GameId}/")]
+        public async Task<ActionResult> NewRecordStatistics(long GameId)
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user != null)
+            {
+                Statistics statistics = new Statistics();
+                return Ok(statistics.RecordSession(Guid.Empty, GameId, user.Id));
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+
+        [MapToApiVersion("1.0")]
+        [MapToApiVersion("1.1")]
+        [HttpPut]
+        [Authorize]
+        [ProducesResponseType(typeof(Models.StatisticsModel), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [Route("Games/{GameId}/{SessionId}")]
+        public async Task<ActionResult> SubsequentRecordStatistics(long GameId, Guid SessionId)
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user != null)
+            {
+                Statistics statistics = new Statistics();
+                return Ok(statistics.RecordSession(SessionId, GameId, user.Id));
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+
+        [MapToApiVersion("1.0")]
+        [MapToApiVersion("1.1")]
+        [HttpGet]
+        [Authorize]
+        [ProducesResponseType(typeof(Models.StatisticsModel), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [Route("Games/{GameId}")]
+        public async Task<ActionResult> GetStatistics(long GameId)
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user != null)
+            {
+                Statistics statistics = new Statistics();
+                StatisticsModel? model = statistics.GetSession(GameId, user.Id);
+                if (model == null)
+                {
+                    return NoContent();
+                }
+                else
+                {
+                    return Ok(model);
+                }
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+    }
+}
