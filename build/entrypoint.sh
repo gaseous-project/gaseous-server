13c5ebbde18575e919b381de41a1df304c63f5ea
Michael Green <84688932+michael-j-green@users.noreply.github.com>
2024-07-12 00:40:10 +1000

    Test embedded docker image

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -3,10 +3,11 @@
 # create the user
 echo "Creating user gaseous with UID ${PUID} and GID ${PGID}"
 groupadd -g ${PGID} gaseous
-useradd -u ${PUID} -g ${PGID} -m gaseous -G sudo 
+useradd -u ${PUID} -g ${PGID} -m gaseous -d /home/gaseous -G sudo 
 usermod -p "*" gaseous
-mkdir -p /home/gaseous/.gaseous-server /var/lib/mysql
-chown -R ${PUID}:${PGID} /App /home/gaseous/.gaseous-server /var/lib/mysql /run/mysqld
+mkdir -p /home/gaseous/.gaseous-server /var/lib/mysql /var/log/mariadb /run/mysqld
+chown -R ${PUID} /App /home/gaseous/.gaseous-server /var/lib/mysql /var/log/mariadb /run/mysqld
+chgrp -R ${PGID} /App /home/gaseous/.gaseous-server /var/lib/mysql /var/log/mariadb /run/mysqld
 
 # Start supervisord and services
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
