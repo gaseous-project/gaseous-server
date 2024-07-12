3366d926f4ec33e5d283a2d29652501882b9df94
Michael Green <84688932+michael-j-green@users.noreply.github.com>
2024-07-12 15:43:02 +1000

    Unraid compatible single container docker image (#390)
    See https://github.com/gaseous-project/unraid-template for an Unraid XML
    template.

--- build/mariadb.sh
+++ build/mariadb.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# install the database
+/usr/bin/mariadb-install-db --datadir=/var/lib/mysql --user=gaseous
+
 # start the database server without network or grant tables
 /usr/sbin/mariadbd --datadir=/var/lib/mysql  --skip-grant-tables --skip-networking &
 
@@ -7,7 +10,7 @@
 sleep 5
 
 # change the root password
-mariadb -u root -e "FLUSH PRIVILEGES; ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PASSWORD'; FLUSH PRIVILEGES;"
+mariadb -u root -e "FLUSH PRIVILEGES; ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PASSWORD'; ALTER USER 'gaseous'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PASSWORD'; FLUSH PRIVILEGES; SHUTDOWN;"
 
 # stop the server
 sleep 5
