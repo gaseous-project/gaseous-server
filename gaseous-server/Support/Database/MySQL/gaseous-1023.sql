90892ce869b6370f6e0ccc255f00d92bcce76a0d
Michael Green <84688932+michael-j-green@users.noreply.github.com>
2024-07-17 23:21:49 +1000

    Update database scripts to match v1.7.5

--- gaseous-server/Support/Database/MySQL/gaseous-1023.sql
+++ gaseous-server/Support/Database/MySQL/gaseous-1023.sql
@@ -1,3 +1,43 @@
+CREATE TABLE `Signatures_RomToSource` (
+    `SourceId` int NOT NULL,
+    `RomId` int NOT NULL,
+    PRIMARY KEY (`SourceId`, `RomId`)
+);
+
+CREATE TABLE `Signatures_Games_Countries` (
+    `GameId` INT NOT NULL,
+    `CountryId` INT NOT NULL,
+    PRIMARY KEY (`GameId`, `CountryId`),
+    CONSTRAINT `GameCountry` FOREIGN KEY (`GameId`) REFERENCES `Signatures_Games` (`Id`) ON DELETE CASCADE ON UPDATE NO ACTION
+);
+
+CREATE TABLE `Signatures_Games_Languages` (
+    `GameId` INT NOT NULL,
+    `LanguageId` INT NOT NULL,
+    PRIMARY KEY (`GameId`, `LanguageId`),
+    CONSTRAINT `GameLanguage` FOREIGN KEY (`GameId`) REFERENCES `Signatures_Games` (`Id`) ON DELETE CASCADE ON UPDATE NO ACTION
+);
+
+CREATE TABLE `Country` (
+    `Id` INT NOT NULL AUTO_INCREMENT,
+    `Code` VARCHAR(20) NULL,
+    `Value` VARCHAR(255) NULL,
+    PRIMARY KEY (`Id`),
+    INDEX `id_Code` (`Code` ASC) VISIBLE,
+    INDEX `id_Value` (`Value` ASC) VISIBLE
+);
+
+CREATE TABLE `Language` (
+    `Id` INT NOT NULL AUTO_INCREMENT,
+    `Code` VARCHAR(20) NULL,
+    `Value` VARCHAR(255) NULL,
+    PRIMARY KEY (`Id`),
+    INDEX `id_Code` (`Code` ASC) VISIBLE,
+    INDEX `id_Value` (`Value` ASC) VISIBLE
+);
+
+ALTER TABLE `Games_Roms` ADD COLUMN `RomDataVersion` INT DEFAULT 1;
+
 CREATE TABLE UserProfiles (
     `Id` VARCHAR(45) NOT NULL,
     `UserId` VARCHAR(45) NOT NULL,
@@ -10,5 +50,3 @@ CREATE TABLE UserProfiles (
     `UnstructuredData` LONGTEXT NOT NULL,
     PRIMARY KEY (`Id`, `UserId`)
 );
-
-ALTER TABLE `Platform` CHANGE `Name` `Name` varchar(255);
