2c290821efdcf7a7fd0645d8c23fbb0c67fa70dd
Michael Green <84688932+michael-j-green@users.noreply.github.com>
2025-07-07 00:26:36 +1000

    DB modifications

--- gaseous-server/Support/Database/MySQL/gaseous-1029.sql
+++ gaseous-server/Support/Database/MySQL/gaseous-1029.sql
@@ -1,15 +1,10 @@
-CREATE INDEX idx_MetadataMap_Id_PlatformId ON MetadataMap (Id, PlatformId);
-
-CREATE INDEX idx_GameSaves_RomId_IsMediaGroup_UserId ON GameSaves (RomId, IsMediaGroup, UserId);
-
-CREATE INDEX idx_AgeGroup_GameId_SourceId_AgeGroupId ON AgeGroup (GameId, SourceId, AgeGroupId);
-
-CREATE INDEX idx_GameLocalization_Game_SourceId_Region ON GameLocalization (Game, SourceId, Region);
-
-CREATE INDEX idx_GameLocalization_Name ON GameLocalization (Name);
-
-CREATE INDEX idx_Region_Id_SourceId_Identifier ON Region (Id, SourceId, Identifier);
-
-CREATE INDEX idx_Game_Id ON Game (Id);
-
-CREATE INDEX idx_Platform_Id_SourceId_Name ON Platform (Id, SourceId, Name);
+CREATE TABLE GameSaves (
+    `Id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `UserId` VARCHAR(45) NOT NULL,
+    `RomId` BIGINT NOT NULL,
+    `IsMediaGroup` TINYINT NOT NULL,
+    `CoreName` VARCHAR(45) NOT NULL,
+    `MD5` VARCHAR(32) NOT NULL,
+    `Timestamp` DATETIME NOT NULL,
+    `File` LONGBLOB NOT NULL
+);
