965badd1b6e4aa54674e6afeaa383d99f674e792
Michael Green <michael.green@mrgtech.net>
2026-01-06 11:14:54 +1100

    Optimize SQL queries and add performance indexes for improved query efficiency

--- gaseous-server/Classes/Filters.cs
+++ gaseous-server/Classes/Filters.cs
@@ -26,21 +26,50 @@ namespace gaseous_server.Classes
             List<FilterItem> returnList = new List<FilterItem>();
 
             // age restriction clauses
-            string ageRestriction_Platform = "AgeGroup.AgeGroupId <= " + (int)MaximumAgeRestriction;
+            string ageRestriction_Platform = "ag.AgeGroupId <= " + (int)MaximumAgeRestriction;
             string ageRestriction_Generic = "view_Games.AgeGroupId <= " + (int)MaximumAgeRestriction;
             if (IncludeUnrated == true)
             {
-                ageRestriction_Platform += " OR AgeGroup.AgeGroupId IS NULL";
+                ageRestriction_Platform += " OR ag.AgeGroupId IS NULL";
                 ageRestriction_Generic += " OR view_Games.AgeGroupId IS NULL";
             }
 
             switch (filterType)
             {
                 case FilterType.Platforms:
-                    sql = "SELECT Platform.Id, Platform.`Name`, COUNT(Game.Id) AS GameCount FROM (SELECT DISTINCT Game.Id, view_Games_Roms.PlatformId, COUNT(view_Games_Roms.Id) AS RomCount FROM `Metadata_Game` AS `Game` LEFT JOIN `Metadata_AgeGroup` AS `AgeGroup` ON Game.Id = AgeGroup.GameId LEFT JOIN view_Games_Roms ON Game.Id = view_Games_Roms.GameId WHERE (" + ageRestriction_Platform + ") GROUP BY Game.Id , view_Games_Roms.PlatformId HAVING RomCount > 0) Game JOIN `Metadata_Platform` AS `Platform` ON Game.PlatformId = Platform.Id AND Platform.SourceId = 0 GROUP BY Platform.`Name`;";
+                    // Optimized query: use CTE with view_MetadataMap
+                    sql = @"
+                        WITH FilteredGames AS (
+                            SELECT DISTINCT 
+                                g.Id,
+                                g.SourceId AS GameIdType
+                            FROM Metadata_Game g
+                            LEFT JOIN Metadata_AgeGroup ag ON g.Id = ag.GameId
+                            WHERE (" + ageRestriction_Platform + @")
+                        ),
+                        GamesWithRoms AS (
+                            SELECT DISTINCT
+                                fg.Id,
+                                gr.PlatformId
+                            FROM FilteredGames fg
+                            INNER JOIN view_MetadataMap vmm 
+                                ON vmm.MetadataSourceId = fg.Id 
+                                AND vmm.MetadataSourceType = fg.GameIdType
+                            INNER JOIN Games_Roms gr ON gr.MetadataMapId = vmm.Id
+                        )
+                        SELECT 
+                            p.Id, 
+                            p.Name, 
+                            COUNT(DISTINCT gwr.Id) AS GameCount
+                        FROM GamesWithRoms gwr
+                        INNER JOIN Metadata_Platform p 
+                            ON gwr.PlatformId = p.Id 
+                            AND p.SourceId = 0
+                        GROUP BY p.Id, p.Name
+                        ORDER BY p.Name";
 
                     DataTable dbResponse = await db.ExecuteCMDAsync(sql, new DatabaseMemoryCacheOptions(CacheEnabled: true, ExpirationSeconds: 300));
-
+                    
                     foreach (DataRow dr in dbResponse.Rows)
                     {
                         FilterItem item = new FilterItem(dr);
@@ -50,9 +79,35 @@ namespace gaseous_server.Classes
                     return returnList;
 
                 case FilterType.AgeGroupings:
-                    sql = "SELECT Game.AgeGroupId, COUNT(Game.Id) AS GameCount FROM (SELECT DISTINCT Game.Id, AgeGroup.AgeGroupId, COUNT(view_Games_Roms.Id) AS RomCount FROM `Metadata_Game` AS `Game` LEFT JOIN `Metadata_AgeGroup` AS `AgeGroup` ON Game.Id = AgeGroup.GameId LEFT JOIN view_Games_Roms ON Game.Id = view_Games_Roms.GameId WHERE (" + ageRestriction_Platform + ") GROUP BY Game.Id HAVING RomCount > 0) Game GROUP BY Game.AgeGroupId ORDER BY Game.AgeGroupId DESC";
+                    // Optimized query: use CTE with view_MetadataMap
+                    sql = @"
+                        WITH FilteredGames AS (
+                            SELECT DISTINCT 
+                                g.Id,
+                                g.SourceId AS GameIdType
+                            FROM Metadata_Game g
+                            LEFT JOIN Metadata_AgeGroup ag ON g.Id = ag.GameId
+                            WHERE (" + ageRestriction_Platform + @")
+                        ),
+                        GamesWithRoms AS (
+                            SELECT DISTINCT
+                                fg.Id,
+                                ag.AgeGroupId
+                            FROM FilteredGames fg
+                            INNER JOIN view_MetadataMap vmm 
+                                ON vmm.MetadataSourceId = fg.Id 
+                                AND vmm.MetadataSourceType = fg.GameIdType
+                            INNER JOIN Games_Roms gr ON gr.MetadataMapId = vmm.Id
+                            LEFT JOIN Metadata_AgeGroup ag ON fg.Id = ag.GameId
+                        )
+                        SELECT 
+                            AgeGroupId, 
+                            COUNT(DISTINCT Id) AS GameCount
+                        FROM GamesWithRoms
+                        GROUP BY AgeGroupId
+                        ORDER BY AgeGroupId DESC";
                     dbResponse = await db.ExecuteCMDAsync(sql, new DatabaseMemoryCacheOptions(CacheEnabled: true, ExpirationSeconds: 300));
-
+                    
                     foreach (DataRow dr in dbResponse.Rows)
                     {
                         FilterItem filterAgeGrouping = new FilterItem();
@@ -109,50 +164,96 @@ namespace gaseous_server.Classes
 
             Dictionary<string, List<FilterItem>> FilterSet = new Dictionary<string, List<FilterItem>>();
 
-            // platforms
-            List<FilterItem> platforms = new List<FilterItem>();
-
-            string ageRestriction_Platform = "AgeGroup.AgeGroupId <= " + (int)MaximumAgeRestriction;
-            string ageRestriction_Generic = "view_Games.AgeGroupId <= " + (int)MaximumAgeRestriction;
+            // Build age restriction clause once
+            string ageRestriction_Platform = "ag.AgeGroupId <= " + (int)MaximumAgeRestriction;
             if (IncludeUnrated == true)
             {
-                ageRestriction_Platform += " OR AgeGroup.AgeGroupId IS NULL";
-                ageRestriction_Generic += " OR view_Games.AgeGroupId IS NULL";
+                ageRestriction_Platform += " OR ag.AgeGroupId IS NULL";
             }
 
-            string sql = "SELECT Platform.Id, Platform.`Name`, COUNT(Game.Id) AS GameCount FROM (SELECT DISTINCT Game.Id, view_Games_Roms.PlatformId, COUNT(view_Games_Roms.Id) AS RomCount FROM `Metadata_Game` AS `Game` LEFT JOIN `Metadata_AgeGroup` AS `AgeGroup` ON Game.Id = AgeGroup.GameId LEFT JOIN view_Games_Roms ON Game.Id = view_Games_Roms.GameId WHERE (" + ageRestriction_Platform + ") GROUP BY Game.Id , view_Games_Roms.PlatformId HAVING RomCount > 0) Game JOIN `Metadata_Platform` AS `Platform` ON Game.PlatformId = Platform.Id AND Platform.SourceId = 0 GROUP BY Platform.`Name`;";
+            // OPTIMIZED: Compute the filtered game set once using a base query
+            // This replaces 6 independent expensive subqueries with 1 base query + 6 lightweight joins
+            string baseFilteredGamesQuery = @"
+                CREATE TEMPORARY TABLE IF NOT EXISTS temp_FilteredGamesWithRoms (
+                    GameId BIGINT,
+                    GameIdType INT,
+                    PlatformId BIGINT,
+                    AgeGroupId INT,
+                    INDEX idx_gameid_type (GameId, GameIdType),
+                    INDEX idx_platformid (PlatformId),
+                    INDEX idx_agegroupid (AgeGroupId)
+                ) ENGINE=MEMORY;
+                
+                TRUNCATE TABLE temp_FilteredGamesWithRoms;
+                
+                INSERT INTO temp_FilteredGamesWithRoms
+                SELECT DISTINCT
+                    g.Id AS GameId,
+                    g.SourceId AS GameIdType,
+                    gr.PlatformId,
+                    ag.AgeGroupId
+                FROM Metadata_Game g
+                LEFT JOIN Metadata_AgeGroup ag ON g.Id = ag.GameId
+                INNER JOIN view_MetadataMap vmm 
+                    ON vmm.MetadataSourceId = g.Id 
+                    AND vmm.MetadataSourceType = g.SourceId
+                INNER JOIN Games_Roms gr ON gr.MetadataMapId = vmm.Id
+                WHERE (" + ageRestriction_Platform + @")";
 
-            DataTable dbResponse = await db.ExecuteCMDAsync(sql, new DatabaseMemoryCacheOptions(CacheEnabled: true, ExpirationSeconds: 300));
+            await db.ExecuteCMDAsync(baseFilteredGamesQuery, new DatabaseMemoryCacheOptions(CacheEnabled: false));
 
+            // Now run lightweight queries against the temp table
+            
+            // platforms
+            List<FilterItem> platforms = new List<FilterItem>();
+            string sql = @"
+                SELECT 
+                    p.Id, 
+                    p.Name, 
+                    COUNT(DISTINCT fg.GameId) AS GameCount
+                FROM temp_FilteredGamesWithRoms fg
+                INNER JOIN Metadata_Platform p 
+                    ON fg.PlatformId = p.Id 
+                    AND p.SourceId = 0
+                GROUP BY p.Id, p.Name
+                ORDER BY p.Name";
+
+            DataTable dbResponse = await db.ExecuteCMDAsync(sql, new DatabaseMemoryCacheOptions(CacheEnabled: false));
+            
             foreach (DataRow dr in dbResponse.Rows)
             {
                 FilterItem platformItem = new FilterItem(dr);
                 platforms.Add(platformItem);
-
             }
             FilterSet.Add("platforms", platforms);
 
             // genres
-            List<FilterItem> genres = await GenerateFilterSet(db, "Genre", ageRestriction_Platform);
+            List<FilterItem> genres = await GenerateFilterSetFromTemp(db, "Genre");
             FilterSet.Add("genres", genres);
 
             // game modes
-            List<FilterItem> gameModes = await GenerateFilterSet(db, "GameMode", ageRestriction_Platform);
+            List<FilterItem> gameModes = await GenerateFilterSetFromTemp(db, "GameMode");
             FilterSet.Add("gamemodes", gameModes);
 
             // player perspectives
-            List<FilterItem> playerPerspectives = await GenerateFilterSet(db, "PlayerPerspective", ageRestriction_Platform);
+            List<FilterItem> playerPerspectives = await GenerateFilterSetFromTemp(db, "PlayerPerspective");
             FilterSet.Add("playerperspectives", playerPerspectives);
 
             // themes
-            List<FilterItem> themes = await GenerateFilterSet(db, "Theme", ageRestriction_Platform);
+            List<FilterItem> themes = await GenerateFilterSetFromTemp(db, "Theme");
             FilterSet.Add("themes", themes);
 
             // age groups
             List<FilterItem> agegroupings = new List<FilterItem>();
-            sql = "SELECT Game.AgeGroupId, COUNT(Game.Id) AS GameCount FROM (SELECT DISTINCT Game.Id, AgeGroup.AgeGroupId, COUNT(view_Games_Roms.Id) AS RomCount FROM `Metadata_Game` AS `Game` LEFT JOIN `Metadata_AgeGroup` AS `AgeGroup` ON Game.Id = AgeGroup.GameId LEFT JOIN view_Games_Roms ON Game.Id = view_Games_Roms.GameId WHERE (" + ageRestriction_Platform + ") GROUP BY Game.Id HAVING RomCount > 0) Game GROUP BY Game.AgeGroupId ORDER BY Game.AgeGroupId DESC";
-            dbResponse = await db.ExecuteCMDAsync(sql, new DatabaseMemoryCacheOptions(CacheEnabled: true, ExpirationSeconds: 300));
-
+            sql = @"
+                SELECT 
+                    AgeGroupId, 
+                    COUNT(DISTINCT GameId) AS GameCount
+                FROM temp_FilteredGamesWithRoms
+                GROUP BY AgeGroupId
+                ORDER BY AgeGroupId DESC";
+            dbResponse = await db.ExecuteCMDAsync(sql, new DatabaseMemoryCacheOptions(CacheEnabled: false));
+            
             foreach (DataRow dr in dbResponse.Rows)
             {
                 FilterItem filterAgeGrouping = new FilterItem();
@@ -173,7 +274,77 @@ namespace gaseous_server.Classes
             }
             FilterSet.Add("agegroupings", agegroupings);
 
+            // Clean up temp table
+            await db.ExecuteCMDAsync("DROP TEMPORARY TABLE IF EXISTS temp_FilteredGamesWithRoms", new DatabaseMemoryCacheOptions(CacheEnabled: false));
+
             return FilterSet;
+        }
+
+        private static async Task<List<FilterItem>> GenerateFilterSetFromTemp(Database db, string Name)
+        {
+            List<FilterItem> filter = new List<FilterItem>();
+            DataTable dbResponse = await GetGenericFilterItemFromTemp(db, Name);
+
+            foreach (DataRow dr in dbResponse.Rows)
+            {
+                FilterItem filterItem = new FilterItem(dr);
+                if (filterItem != null)
+                {
+                    bool nameExists = false;
+                    foreach (var filterObject in filter)
+                    {
+                        if (filterObject.Name == filterItem.Name)
+                        {
+                            // add the ids to the existing genre
+                            if (filterObject.Ids == null)
+                            {
+                                filterObject.Ids = new Dictionary<HasheousClient.Models.MetadataSources, long>();
+                            }
+
+                            foreach (var id in filterItem.Ids)
+                            {
+                                if (filterObject.Ids.ContainsKey(id.Key) == false)
+                                {
+                                    filterObject.Ids.Add(id.Key, id.Value);
+                                    filterObject.GameCount += filterItem.GameCount;
+                                }
+                            }
+
+                            nameExists = true;
+                        }
+                    }
+
+                    if (nameExists == false)
+                    {
+                        filter.Add(filterItem);
+                    }
+                }
+            }
+
+            return filter;
+        }
+
+        private static async Task<DataTable> GetGenericFilterItemFromTemp(Database db, string Name)
+        {
+            // Lightweight query against the pre-computed temp table
+            string sql = @"
+                SELECT 
+                    fg.GameIdType, 
+                    item.Id, 
+                    item.Name, 
+                    COUNT(DISTINCT fg.GameId) AS GameCount
+                FROM temp_FilteredGamesWithRoms fg
+                INNER JOIN Relation_Game_<ITEMNAME>s rel
+                    ON fg.GameId = rel.GameId 
+                    AND fg.GameIdType = rel.GameSourceId
+                INNER JOIN Metadata_<ITEMNAME> item 
+                    ON rel.<ITEMNAME>sId = item.Id
+                GROUP BY fg.GameIdType, item.Id, item.Name
+                ORDER BY item.Name";
+            sql = sql.Replace("<ITEMNAME>", Name);
+            DataTable dbResponse = await db.ExecuteCMDAsync(sql, new DatabaseMemoryCacheOptions(CacheEnabled: false));
+            
+            return dbResponse;
         }
 
         private static async Task<List<FilterItem>> GenerateFilterSet(Database db, string Name, string AgeRestriction)
@@ -222,10 +393,42 @@ namespace gaseous_server.Classes
 
         private static async Task<DataTable> GetGenericFilterItem(Database db, string Name, string AgeRestriction)
         {
-            string sql = "SELECT Game.GameIdType, <ITEMNAME>.Id, <ITEMNAME>.`Name`, COUNT(Game.Id) AS GameCount FROM (SELECT DISTINCT view_Games_Roms.GameIdType, Game.Id, AgeGroup.AgeGroupId, COUNT(view_Games_Roms.Id) AS RomCount FROM `Metadata_Game` AS `Game` LEFT JOIN `Metadata_AgeGroup` AS `AgeGroup` ON Game.Id = AgeGroup.GameId LEFT JOIN view_Games_Roms ON Game.Id = view_Games_Roms.GameId WHERE (" + AgeRestriction + ") GROUP BY Game.Id HAVING RomCount > 0) Game JOIN Relation_Game_<ITEMNAME>s ON Game.Id = Relation_Game_<ITEMNAME>s.GameId AND Game.GameIdType = Relation_Game_<ITEMNAME>s.GameSourceId JOIN `Metadata_<ITEMNAME>` AS `<ITEMNAME>` ON Relation_Game_<ITEMNAME>s.<ITEMNAME>sId = <ITEMNAME>.Id GROUP BY GameIdType, <ITEMNAME>.`Name` ORDER BY <ITEMNAME>.`Name`;";
+            // Optimized query: use CTE with view_MetadataMap
+            string sql = @"
+                WITH FilteredGames AS (
+                    SELECT DISTINCT 
+                        g.Id,
+                        g.SourceId AS GameIdType
+                    FROM Metadata_Game g
+                    LEFT JOIN Metadata_AgeGroup ag ON g.Id = ag.GameId
+                    WHERE (" + AgeRestriction + @")
+                ),
+                GamesWithRoms AS (
+                    SELECT DISTINCT
+                        fg.Id,
+                        fg.GameIdType
+                    FROM FilteredGames fg
+                    INNER JOIN view_MetadataMap vmm 
+                        ON vmm.MetadataSourceId = fg.Id 
+                        AND vmm.MetadataSourceType = fg.GameIdType
+                    INNER JOIN Games_Roms gr ON gr.MetadataMapId = vmm.Id
+                )
+                SELECT 
+                    gwr.GameIdType, 
+                    item.Id, 
+                    item.Name, 
+                    COUNT(DISTINCT gwr.Id) AS GameCount
+                FROM GamesWithRoms gwr
+                INNER JOIN Relation_Game_<ITEMNAME>s rel
+                    ON gwr.Id = rel.GameId 
+                    AND gwr.GameIdType = rel.GameSourceId
+                INNER JOIN Metadata_<ITEMNAME> item 
+                    ON rel.<ITEMNAME>sId = item.Id
+                GROUP BY gwr.GameIdType, item.Id, item.Name
+                ORDER BY item.Name";
             sql = sql.Replace("<ITEMNAME>", Name);
             DataTable dbResponse = await db.ExecuteCMDAsync(sql, new DatabaseMemoryCacheOptions(CacheEnabled: true, ExpirationSeconds: 300));
-
+            
             return dbResponse;
         }
 
--- gaseous-server/Support/Database/MySQL/gaseous-1035.sql
+++ gaseous-server/Support/Database/MySQL/gaseous-1035.sql
@@ -1,4 +1,3 @@
--- Performance indexes for GetGames query
 ALTER TABLE UserTimeTracking
 ADD KEY idx_GameId_PlatformId_UserId (GameId, PlatformId, UserId);
 
@@ -6,3 +5,37 @@ ALTER TABLE RomMediaGroup
 ADD KEY idx_GameId_PlatformId (GameId, PlatformId);
 
 ALTER TABLE Games_Roms ADD KEY idx_MetadataMapId (MetadataMapId);
+
+CREATE INDEX idx_Relation_GameModes_GameId_SourceId ON Relation_Game_GameModes (
+    GameId,
+    GameSourceId,
+    GameModesId
+);
+
+CREATE INDEX idx_Relation_PlayerPerspectives_GameId_SourceId ON Relation_Game_PlayerPerspectives (
+    GameId,
+    GameSourceId,
+    PlayerPerspectivesId
+);
+
+CREATE INDEX idx_Relation_Themes_GameId_SourceId ON Relation_Game_Themes (
+    GameId,
+    GameSourceId,
+    ThemesId
+);
+
+CREATE INDEX IF NOT EXISTS idx_Relation_Genres_composite ON Relation_Game_Genres (
+    GameId,
+    GameSourceId,
+    GenresId
+);
+
+CREATE INDEX idx_Games_Roms_GameId ON Games_Roms (GameId);
+
+CREATE INDEX idx_Games_Roms_MetadataMapId ON Games_Roms (MetadataMapId);
+
+CREATE INDEX idx_Metadata_Game_Id_SourceId ON Metadata_Game (Id, SourceId);
+
+CREATE INDEX IF NOT EXISTS idx_Games_Roms_MetadataMapId_PlatformId ON Games_Roms (MetadataMapId, PlatformId);
+
+CREATE INDEX IF NOT EXISTS idx_Metadata_Platform_Id_SourceId ON Metadata_Platform (Id, SourceId);
