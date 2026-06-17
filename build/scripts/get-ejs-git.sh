ea0dffc7777aa147c904533fceea98d029646d48
Michael Green <84688932+michael-j-green@users.noreply.github.com>
2026-06-18 00:06:08 +1000

    Fix Dockerfile to copy EmulatorJS files instead of moving them

--- build/scripts/get-ejs-git.sh
+++ build/scripts/get-ejs-git.sh
@@ -13,22 +13,22 @@ git submodule update --init --recursive --remote --force "$SUBREPO_DIR"
 git submodule set-branch --branch "$TARGET_BRANCH" "$SUBREPO_DIR"
 git submodule update --init --recursive --remote --force "$SUBREPO_DIR"
 
-# Recursively mirror all core files from the CDN into the local cores directory.
-# This will overwrite existing files but will not delete extra local files.
-# If you want a clean sync, delete the destination directory first.
-CORES_URL="https://cdn.emulatorjs.org/nightly/data/cores/"
-DEST_DIR="./gaseous-server/wwwroot/emulators/EmulatorJS/data/cores"
+# # Recursively mirror all core files from the CDN into the local cores directory.
+# # This will overwrite existing files but will not delete extra local files.
+# # If you want a clean sync, delete the destination directory first.
+# CORES_URL="https://cdn.emulatorjs.org/nightly/data/cores/"
+# DEST_DIR="./gaseous-server/wwwroot/emulators/EmulatorJS/data/cores"
 
-mkdir -p "$DEST_DIR"
+# mkdir -p "$DEST_DIR"
 
-# Use wget recursive download:
-# -r        : recursive
-# -np       : no parent (stay within cores/)
-# -nH       : don't create host directory
-# --cut-dirs=3 : strip 'nightly/data/cores' from path so deeper structure starts at cores root
-# -R "index.html*" : skip auto-generated index listings
-# -P DEST_DIR : set destination prefix
-# Existing files are overwritten by default.
-wget -r -np -nH --cut-dirs=3 -R "index.html*" -P "$DEST_DIR" "$CORES_URL"
+# # Use wget recursive download:
+# # -r        : recursive
+# # -np       : no parent (stay within cores/)
+# # -nH       : don't create host directory
+# # --cut-dirs=3 : strip 'nightly/data/cores' from path so deeper structure starts at cores root
+# # -R "index.html*" : skip auto-generated index listings
+# # -P DEST_DIR : set destination prefix
+# # Existing files are overwritten by default.
+# wget -r -np -nH --cut-dirs=3 -R "index.html*" -P "$DEST_DIR" "$CORES_URL"
 
-echo "EmulatorJS cores download complete into $DEST_DIR"
+# echo "EmulatorJS cores download complete into $DEST_DIR"
